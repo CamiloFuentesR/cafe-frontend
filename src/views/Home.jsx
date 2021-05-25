d1c6ee249683c6f34be73520da255dbe7a5331a9
CamiloFuentesR <lokomilo1@gmail.com>
2021-05-25 10:24:02 -0400

    home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import {Footer} from '../components/layouts/Footer'
+import React from 'react';
+import heartcoffe from "../styles/img/heartcoffe.jpg"
+import { Footer } from '../components/layouts/Footer';
 export const Home = () => {
     return (
         <div className='__home_screen'>
             <header className="__home_header">
                 <h1>header</h1>
             </header>
-            <section>
-                <h2>section1</h2>
+            <section className="__home_section1">
+                <div className="_home_section1_sub">
+                    <img src={heartcoffe} alt="" />
+                    <h2>Disfruta del mejor Café</h2>
+                </div>
             </section>
-            <section>
+            <section className="__home_menu">
                 <h2>section2</h2>
             </section>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 export const Products = () => {
     return (
         <div>
